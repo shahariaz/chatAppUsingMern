7fd625dceef7cf240a10b3f6ba2741e1d5f5375b
shahariaz <shahariaz.info@gmail.com>
2023-08-21 08:02:06 +0600

    creating something

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-500">testing</div>
+      <Register />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Register.jsx
@@ -0,0 +1,12 @@
+const Register = () => {
+  return (
+    <div className="bg-blue-50 h-screen">
+      <form action="">
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
